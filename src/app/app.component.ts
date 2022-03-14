3d96c62071087fbe8d5612c4328febf0323be709
NethmiWeedagama <60633923+NethmiWeedagama@users.noreply.github.com>
2022-03-14 18:33:00 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  courses= COURSES;
+  title= COURSES[0].description;
+  price=9.99;
+  rate=0.67;
+  startDate=new Date(2000,0,1)
+  // coreCourse= COURSES[0];
+  // rxjsCourse= COURSES[1];
+  // ngrxCourse= COURSES[2];
+
+  onCourseSelected( course:Course){
+     console.log("app component ; click event bubbled")
+  }
+
+}
